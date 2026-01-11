fabe687ba5eb356794ed62557fb6160d1bc62460
annamaulina654 <annamaulina14@gmail.com>
2026-01-12 06:24:25 +0700

    feat: setup database schema and models for announcements and activities

---
+++ app/Models/Activity.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'category',
+        'date',
+        'location',
+        'images',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'images' => 'array', 
+    ];
+}
---
+++ app/Models/Announcement.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Announcement extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'type',
+        'date',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'is_active' => 'boolean',
+    ];
+}
